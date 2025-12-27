fc9af81ecce20083be152cac63af8c75d74e79d0
Oyne <andreoyy.litvinovv@gmail.com>
2025-12-27 22:02:02 +0200

    Merge PDF page POM added

---
+++ pages/merge_page.py
@@ -0,0 +1,14 @@
+import config
+import allure
+from playwright.sync_api import Page
+from pages.base_pdf_page import BasePDFPage
+from pages.elements.base_element import BaseElement
+
+
+class MergePage(BasePDFPage):
+    def __init__(self, page: Page):
+        super().__init__(page, "Merge page", url=config.MERGE_URL)
+        self._merge_button = BaseElement(page, "#mergeBtn")
+
+    def merge_button_is_clickable(self) -> bool:
+        return self._merge_button.is_clickable()
